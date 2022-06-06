ad938fbc0ece59109f6959d0be82766ca8625585
mattBtindall <matthewbtindall@gmail.com>
2022-06-06 12:27:54 +0100

    Add layout with template (renders phtml template)

---
+++ app/code/Mattbtindall/HelloWorldMVVM/Block/Main.php
@@ -0,0 +1,11 @@
+<?php
+namespace Mattbtindall\HelloWorldMVVM\Block;
+use Magento\Framework\View\Element\Template;
+
+class Main extends Template
+{
+    protected function _prepareLayout()
+    {
+
+    }
+}
