71190f2791dcb7e81e90f60782a61ba1b6302209
mattBtindall <matthewbtindall@gmail.com>
2022-06-06 12:49:52 +0100

    Add extra output properties to the content.phtml file with functions from the block class

--- app/code/Mattbtindall/HelloWorldMVVM/Block/Main.php
+++ app/code/Mattbtindall/HelloWorldMVVM/Block/Main.php
@@ -4,8 +4,15 @@ use Magento\Framework\View\Element\Template;
 
 class Main extends Template
 {
+
     protected function _prepareLayout()
     {
+        $this->setMessage('Hello');
+        $this->setName($this->getRequest()->getParam('name'));
+    }
 
+    public function getGoodbyeMessage()
+    {
+        return 'Goodbye World';
     }
 }
