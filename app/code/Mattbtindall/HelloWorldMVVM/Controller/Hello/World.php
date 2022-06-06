f11a3892368c63dc465c6411d5c60992fe15c252
mattBtindall <matthewbtindall@gmail.com>
2022-06-06 11:36:08 +0100

    Add simple controller to display message (doesn't render any html)

---
+++ app/code/Mattbtindall/HelloWorldMVVM/Controller/Hello/World.php
@@ -0,0 +1,10 @@
+<?php
+namespace Mattbtindall\HelloWorldMVVM\Controller\Hello;
+class World extends \Magento\Framework\App\Action\Action
+{
+    public function execute()
+    {
+        echo '<p>You Did It!</p>';
+        var_dump(__METHOD__);
+    }
+}
