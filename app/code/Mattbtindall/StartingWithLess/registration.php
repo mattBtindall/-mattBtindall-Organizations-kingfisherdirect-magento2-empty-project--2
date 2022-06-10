4647aa70da3d2d06ca4d443833860352e7d1aab5
mattBtindall <matthewbtindall@gmail.com>
2022-06-10 12:16:55 +0100

    Complete tutorial

---
+++ app/code/Mattbtindall/StartingWithLess/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Mattbtindall_StartingWithLess',
+    __DIR__
+);
